135582883d6b41fa26a317c7b34a6b58db57c609
faceyacc <justfacey@gmail.com>
2024-07-13 19:18:38 -0400

    added proper logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import sys
 
 from dataclasses import dataclass
+from loguru import logger
+
 
 # import sqlparse - available if you need it!
 
@@ -10,11 +12,11 @@ command = sys.argv[2]
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
         # You can use print statements as follows for debugging, they'll be visible when running tests.
-        print("Logs from your program will appear here!")
+        logger.debug("Logs from your program will appear here!")
 
         # Uncomment this to pass the first stage
         # database_file.seek(16)  # Skip the first 16 bytes of the header
         # page_size = int.from_bytes(database_file.read(2), byteorder="big")
         # print(f"database page size: {page_size}")
 else:
-    print(f"Invalid command: {command}")
+    logger.debug(f"Invalid command: {command}")
