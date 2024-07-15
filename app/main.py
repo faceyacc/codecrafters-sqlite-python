8364cdea202b9a75f09a2b2c428c58d6e285a5b8
faceyacc <justfacey@gmail.com>
2024-07-15 05:17:24 -0400

    implemented helper function and .tables command to print table names

--- app/main.py
+++ app/main.py
@@ -1,12 +1,39 @@
 import sys
+import re
 
 from dataclasses import dataclass
+from typing import List
 
-
-# import sqlparse - available if you need it!
-
+# Get commands from command line
 database_file_path = sys.argv[1]
 command = sys.argv[2]
+
+
+
+def get_table_names(database_file):
+    # Define pattern to match b'CREATE TABLE' statements using regex
+    pattern = rb'CREATE TABLE (\w+)'
+
+    f = database_file.read()
+
+    # Find all tables in the database file using regex pattern
+    tables = re.findall(pattern, f)
+
+    tables = sorted([table.decode() for table in tables])
+    tables = ' '.join(tables)
+
+    return tables
+
+
+if command == ".tables":
+    with open(database_file_path, "rb") as database_file:
+
+        database_file.seek(16)  # Skip the first 16 bytes of the header
+
+        # Get list of names of tables in the database
+        names = get_table_names(database_file)
+
+        print(names)
 
 if command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
@@ -23,9 +50,11 @@ if command == ".dbinfo":
 
 
 
-
-
         print(f"database page size: {page_size}")
         print(f"number of tables: {number_of_tables}")
+
+
+
+
 else:
     print(f"Invalid command: {command}")
