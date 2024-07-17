91019d792eee286e97b30626e5177cedf5f50b10
faceyacc <justfacey@gmail.com>
2024-07-17 06:30:35 -0400

    added helper function to print b-tree header

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ database_file_path = sys.argv[1]
 command = sys.argv[2]
 
 
-
+# Helper function to get list of table names from database file
 def get_table_names(database_file):
     # Define pattern to match b'CREATE TABLE' statements using regex
     pattern = rb'CREATE TABLE (\w+)'
@@ -23,6 +23,49 @@ def get_table_names(database_file):
     tables = ' '.join(tables)
 
     return tables
+
+
+# Helper function to print b-tree page header
+def print_b_tree_page_header(database_file):
+
+    b_tree_type = int.from_bytes(database_file.read(1), byteorder="big")
+
+    database_file.seek(1)
+    first_freeblock = int.from_bytes(database_file.read(2), byteorder="big")
+
+    # Read bytes at offset 3
+    database_file.seek(3)
+    number_of_cells = int.from_bytes(database_file.read(2), byteorder="big")
+
+    database_file.seek(5)
+    start_of_cell_content_area = int.from_bytes(database_file.read(2), byteorder="big")
+
+    database_file.seek(8)
+    right_pointer = int.from_bytes(database_file.read(4), byteorder="big")
+
+
+    print(f"B-tree type -> {b_tree_type}\nFirst Free Block -> {first_freeblock}\nNumber of Cells on Page -> {number_of_cells}\nStart of Cell Content -> {start_of_cell_content_area}\nRight-most Pointer -> {right_pointer}\n")
+
+
+
+
+# Check if command starts with "SELECT"
+if command.startswith("select"):
+
+    # Get table name from SELECT command
+    table_name = command.split(" ")[3]
+
+    with open(database_file_path, "rb") as database_file:
+
+        # The 100-byte database file header is found only on page 1,
+        # which is always a table b-tree page. All other b-tree pages
+        # in the database file omit this 100-byte header.
+        database_file.seek(100)
+        print_b_tree_page_header(database_file)
+
+
+
+
 
 
 if command == ".tables":
