5a3ce4ca9104b816cf42a6eeb58f2c8d3cac56aa
faceyacc <justfacey@gmail.com>
2024-07-19 16:11:54 -0400

    Implemented select count(*) query to return number of row for a given table.

--- app/main.py
+++ app/main.py
@@ -9,6 +9,11 @@ database_file_path = sys.argv[1]
 command = sys.argv[2]
 
 
+# Helper func to read int from bytes
+def read_int(database_file, size):
+    return int.from_bytes(database_file.read(size), byteorder="big")
+
+
 # Helper function to get list of table names from database file
 def get_table_names(database_file):
     # Define pattern to match b'CREATE TABLE' statements using regex
@@ -26,45 +31,99 @@ def get_table_names(database_file):
 
 
 # Helper function to print b-tree page header
-def print_b_tree_page_header(database_file):
+def print_table_amount(database_file):
 
-    b_tree_type = int.from_bytes(database_file.read(1), byteorder="big")
-
-    database_file.seek(1)
-    first_freeblock = int.from_bytes(database_file.read(2), byteorder="big")
-
-    # Read bytes at offset 3
-    database_file.seek(3)
-    number_of_cells = int.from_bytes(database_file.read(2), byteorder="big")
-
-    database_file.seek(5)
-    start_of_cell_content_area = int.from_bytes(database_file.read(2), byteorder="big")
-
-    database_file.seek(8)
-    right_pointer = int.from_bytes(database_file.read(4), byteorder="big")
+    # 1. Get page size from database file
+    # 2. Get number of cells on page
+    # 3. Calculate cell_pointers using right most pointer
 
 
-    print(f"B-tree type -> {b_tree_type}\nFirst Free Block -> {first_freeblock}\nNumber of Cells on Page -> {number_of_cells}\nStart of Cell Content -> {start_of_cell_content_area}\nRight-most Pointer -> {right_pointer}\n")
+    database_file.seek(16)
+    page_size = read_int(database_file, 2)
+
+    database_file.seek(103)
+    number_of_cells = read_int(database_file, 2)
+
+    database_file.seek(108)
+
+    # Get right most pointer
+    # right_most_pointer = read_int(database_file, 2)
+    cell_pointers = [read_int(database_file, 2) for _ in range(number_of_cells)]
+    records = [parse_cell(cell_pointer, database_file) for cell_pointer in cell_pointers]
+    table_info = {record[2]: record[3] for record in records if record[2] != "sqlite_sequence"}
 
 
+    table_page = table_info[table_name.encode('utf-8')]
+
+    database_file.seek(((table_page - 1) * page_size) + 3)
+    table_cell_amount = read_int(database_file, 2)
+    return table_cell_amount
 
 
-# Check if command starts with "SELECT"
-if command.startswith("select"):
+def read_varint(database_file):
+    val = 0
+    USE_NEXT_BYTE = 0x80
+    BITS_TO_USE = 0x7F
 
-    # Get table name from SELECT command
-    table_name = command.split(" ")[3]
-
-    with open(database_file_path, "rb") as database_file:
-
-        # The 100-byte database file header is found only on page 1,
-        # which is always a table b-tree page. All other b-tree pages
-        # in the database file omit this 100-byte header.
-        database_file.seek(100)
-        print_b_tree_page_header(database_file)
+    for _ in range(9):
+        byte = read_int(database_file, 1)
+        val = (val << 7) | (byte & BITS_TO_USE)
+        if byte & USE_NEXT_BYTE == 0:
+            break
+    return val
 
 
+def parse_record(serial_type, database_file):
+    if serial_type == 0:
+        return None
+    elif serial_type == 1:
+        return read_int(database_file, 1)
+    elif serial_type == 2:
+        return read_int(database_file, 2)
+    elif serial_type == 3:
+        return read_int(database_file, 3)
+    elif serial_type == 4:
+        return read_int(database_file, 4)
+    elif serial_type == 5:
+        return read_int(database_file, 6)
+    elif serial_type == 6:
+        return read_int(database_file, 8)
+    elif serial_type == 7:
+        return read_int(database_file, 8)
+    elif serial_type >= 12 and serial_type % 2 == 0:
+        data_len = (serial_type - 12) // 2
+        return database_file.read(data_len).decode()
+    elif serial_type >= 13 and serial_type % 2 == 1:
+        data_len = (serial_type - 13) // 2
+        return database_file.read(data_len)
+    else:
+        print(f"Unknown serial type: {serial_type}")
+        return None
 
+
+def parse_cell(cell_pointer, database_file):
+    # Go to right most pointer
+    database_file.seek(cell_pointer)
+
+    payload_size = read_varint(database_file)
+
+    row_id = read_varint(database_file)
+
+    format_header_start = database_file.tell() # get current file position
+
+    format_header_size = read_varint(database_file)
+
+    serial_types = []
+
+    format_body_start = format_header_start + format_header_size
+
+    while database_file.tell() < format_body_start:
+        serial_types.append(read_varint(database_file))
+
+    records = []
+    for serial_type in serial_types:
+        records.append(parse_record(serial_type, database_file))
+    return records
 
 
 
@@ -78,7 +137,7 @@ if command == ".tables":
 
         print(names)
 
-if command == ".dbinfo":
+elif command == ".dbinfo":
     with open(database_file_path, "rb") as database_file:
 
         print("Logs from your program will appear here!")
@@ -96,8 +155,17 @@ if command == ".dbinfo":
         print(f"database page size: {page_size}")
         print(f"number of tables: {number_of_tables}")
 
+# Check if command starts with "SELECT"
+elif command.lower().startswith("select"):
 
+    query = command.lower().split()
 
+    # Get table name from SELECT command
+    table_name = query[-1]
+
+    with open(database_file_path, "rb") as database_file:
+        table_amount = print_table_amount(database_file)
+        print(table_amount)
 
 else:
     print(f"Invalid command: {command}")
