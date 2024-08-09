d7241936058452dd9010e06df43bcfd51241fc81
faceyacc <justfacey@gmail.com>
2024-08-09 08:06:19 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -18,38 +18,6 @@ command = sys.argv[2]
 statement = sqlparse.split(command)[0].lower()
 table_name = statement.split()[-1]
 
-ON_STG8 = False
-
-# Class to indicate the b-tree page type
-class PageType:
-    InteriorIndex = 0x02
-    InteriorTable = 0x05
-    LeafIndex = 0x0A
-    LeafTable = 0x0D
-
-
-def travel_pages(pg_num, pgsz, db_file, tdesc, query_ref):
-    db_file.seek(pg_num)
-    page_type = read_int(db_file, 1)
-    db_file.seek(pg_num + 3)
-    cell_amt = read_int(db_file, 2)
-    db_file.seek(pg_num + (12 if page_type & 8 == 0 else 8))
-    cell_ptrs = [read_int(db_file, 2) for _ in range(cell_amt)]
-    if page_type == PageType.InteriorTable:
-        global ON_STG8
-        ON_STG8 = True
-        records = []
-        for c_ptr in cell_ptrs:
-            db_file.seek(pg_num + c_ptr)
-            page_num = read_int(db_file, 4)
-            key = read_varint(db_file)
-            records.extend(
-                travel_pages((page_num - 1) * pgsz, pgsz, db_file, tdesc, query_ref) # pyright: ignore
-            )
-        return records
-    elif page_type == PageType.LeafTable:
-        return get_recs(pg_num, cell_ptrs, db_file, tdesc, query_ref)
-
 
 
 if command == ".dbinfo":
--- app/utils.py
+++ app/utils.py
@@ -6,6 +6,12 @@ from typing import Dict
 import binascii
 from .parser import *
 
+# Class to indicate the b-tree page type
+class PageType:
+    # InteriorIndex = 0x02
+    InteriorTable = 0x05
+    # LeafIndex = 0x0A
+    LeafTable = 0x0D
 
 def get_table_info(cell_pointers, database_file, table_name):
     for cell_pointer in cell_pointers:
@@ -22,8 +28,6 @@ def get_table_info(cell_pointers, database_file, table_name):
                 )
             }
 
-
-
 def get_recs(start_offset, cells, database_file, tdesc, query_ref):
     records = []
     for cell_pointer in cells:
@@ -39,3 +43,25 @@ def get_recs(start_offset, cells, database_file, tdesc, query_ref):
                 continue
         records.append(list(record.values()))
     return records
+
+
+def travel_pages(pg_num, pgsz, db_file, tdesc, query_ref):
+    db_file.seek(pg_num)
+    page_type = read_int(db_file, 1)
+    db_file.seek(pg_num + 3)
+    cell_amt = read_int(db_file, 2)
+    db_file.seek(pg_num + (12 if page_type & 8 == 0 else 8))
+    cell_ptrs = [read_int(db_file, 2) for _ in range(cell_amt)]
+
+    if page_type == PageType.InteriorTable:
+        records = []
+        for c_ptr in cell_ptrs:
+            db_file.seek(pg_num + c_ptr)
+            page_num = read_int(db_file, 4)
+            key = read_varint(db_file)
+            records.extend(
+                travel_pages((page_num - 1) * pgsz, pgsz, db_file, tdesc, query_ref) # pyright: ignore
+            )
+        return records
+    elif page_type == PageType.LeafTable:
+        return get_recs(pg_num, cell_ptrs, db_file, tdesc, query_ref)
